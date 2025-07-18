06a7a620f5440bb50b454d1602153a1ea8c5fa59
Mattrade <matthewlestrade@gmail.com>
2025-07-18 14:39:54 +0200

    Delete utils directory

--- utils/ocr.py
+++
@@ -1,8 +0,0 @@
-import pdfplumber
-
-def extract_text_from_pdf(uploaded_file):
-    text = ""
-    with pdfplumber.open(uploaded_file) as pdf:
-        for page in pdf.pages:
-            text += page.extract_text() or ""
-    return text
