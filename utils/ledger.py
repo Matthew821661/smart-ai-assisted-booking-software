712138d0a65e60ac8df78f28cf79989d3837b5bc
Mattrade <matthewlestrade@gmail.com>
2025-07-18 16:04:02 +0200

    Delete utils directory

--- utils/ledger.py
+++
@@ -1,6 +0,0 @@
-
-def generate_ledger_from_ai(df):
-    ledger = df[['Date', 'Description', 'Amount', 'Account', 'VAT Rate']].copy()
-    ledger['Debit'] = ledger['Amount'].apply(lambda x: x if x > 0 else 0)
-    ledger['Credit'] = ledger['Amount'].apply(lambda x: -x if x < 0 else 0)
-    return ledger
