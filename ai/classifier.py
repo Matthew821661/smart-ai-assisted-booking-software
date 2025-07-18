5738aecd4c4e9bb88fc7ee284923441bd927d27f
Mattrade <matthewlestrade@gmail.com>
2025-07-18 16:03:44 +0200

    Delete ai directory

--- ai/classifier.py
+++
@@ -1,5 +0,0 @@
-
-def classify_transactions(df):
-    df['Account'] = df['Description'].apply(lambda x: "Sales" if "sale" in x.lower() else "General Expenses")
-    df['VAT Rate'] = 15
-    return df
